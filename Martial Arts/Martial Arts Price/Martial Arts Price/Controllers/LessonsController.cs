05e07fd3a0875e04d699e3344cf705ea8890c2b3
NasirMohamed <31848184+NasirMohamed@users.noreply.github.com>
2019-02-15 09:55:57 +0000

    Initial commit

---
+++ Martial Arts/Martial Arts Price/Martial Arts Price/Controllers/LessonsController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Martial_Arts_Price.Models;
+
+namespace Martial_Arts_Price.Controllers
+{
+    public class LessonsController : Controller
+    {
+        private martsprEntities db = new martsprEntities();
+
+        // GET: Lessons
+        public ActionResult Index()
+        {
+            var lessons = db.Lessons.Include(l => l.Style).Include(l => l.Teacher);
+            return View(lessons.ToList());
+        }
+
+        // GET: Lessons/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Lesson lesson = db.Lessons.Find(id);
+            if (lesson == null)
+            {
+                return HttpNotFound();
+            }
+            return View(lesson);
+        }
+
+        // GET: Lessons/Create
+        public ActionResult Create()
+        {
+            ViewBag.StyleID = new SelectList(db.Styles, "ID", "Name");
+            ViewBag.TeacherID = new SelectList(db.Teachers, "ID", "Name");
+            return View();
+        }
+
+        // POST: Lessons/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,Image,ImageUpload,Info,StyleID,TeacherID,Price")] Lesson lesson)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Lessons.Add(lesson);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.StyleID = new SelectList(db.Styles, "ID", "Name", lesson.StyleID);
+            ViewBag.TeacherID = new SelectList(db.Teachers, "ID", "Name", lesson.TeacherID);
+            return View(lesson);
+        }
+
+        // GET: Lessons/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Lesson lesson = db.Lessons.Find(id);
+            if (lesson == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.StyleID = new SelectList(db.Styles, "ID", "Name", lesson.StyleID);
+            ViewBag.TeacherID = new SelectList(db.Teachers, "ID", "Name", lesson.TeacherID);
+            return View(lesson);
+        }
+
+        public ActionResult Purchase(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Lesson lesson = db.Lessons.Find(id);
+            if(lesson == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.StyleID = new SelectList(db.Styles, "ID", "Name", lesson.StyleID);
+            ViewBag.TeacherID = new SelectList(db.Teachers, "ID", "Name", lesson.TeacherID);
+            return View(lesson);
+        }
+
+        // POST: Lessons/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,Image,ImageUpload,Info,StyleID,TeacherID,Price")] Lesson lesson)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(lesson).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.StyleID = new SelectList(db.Styles, "ID", "Name", lesson.StyleID);
+            ViewBag.TeacherID = new SelectList(db.Teachers, "ID", "Name", lesson.TeacherID);
+            return View(lesson);
+        }
+
+        // GET: Lessons/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Lesson lesson = db.Lessons.Find(id);
+            if (lesson == null)
+            {
+                return HttpNotFound();
+            }
+            return View(lesson);
+        }
+
+        // POST: Lessons/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Lesson lesson = db.Lessons.Find(id);
+            db.Lessons.Remove(lesson);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Martial Arts/Martial Arts Price/Martial Arts Price/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Martial_Arts_Price.MvcApplication" Language="C#" %>
